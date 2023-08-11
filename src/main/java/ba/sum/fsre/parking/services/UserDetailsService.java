d206057970009803badce9ef3f6fc3d16ec0f383
muhamed1ism <muhamed1ism@gmail.com>
2023-08-12 00:37:40 +0200

    Login and register form with user list

---
+++ src/main/java/ba/sum/fsre/parking/services/UserDetailsService.java
@@ -0,0 +1,24 @@
+package ba.sum.fsre.parking.services;
+
+
+import ba.sum.fsre.parking.model.User;
+import ba.sum.fsre.parking.model.UserDetails;
+import ba.sum.fsre.parking.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserDetails(user);
+    }
+
+}
